b6e52a5df5b2c197c2c7f9dedd43a1ee02ea66a6
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-08 14:47:38 +0300

    Create HAHA.py

---
+++ HAHA.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, types
+from .. import loader, utils
+import random
+
+@loader.module("HAHA", "ecXbe")
+class NotesMod(loader.Module):
+  
+  """Ржач полнейший, использовать -haha <кол-во/random>"""
+  
+  async def haha_cmd(self, app: Client, message: types.Message, args: str):
+    hah = ['п', 'х', 'а', 'в', 'ф', 'ы'] # 22
+    if args:
+      if args.isdigit():
+        for _ in range(int(args)):
+          lol = "".join([random.choice(hah) for _ in range(random.randint(6, 22))])
+          while 'пп' in lol or 'хх' in lol or 'аа' in lol or 'вв' in lol or 'фф' in lol or 'ыы' in lol:
+	          lol = lol.replace('пп', 'п').replace('хх', 'х').replace('аа', 'а').replace('вв', 'в').replace('фф', 'ф').replace('ыы', 'ы')
+          if reply:
+            await reply.reply(lol)
+          else:
+            await app.send_message(message.chat.id, lol)
+      else:
+        if args == "random"
+          for _ in range(random.randint(2, 10)):
+            lol = "".join([random.choice(hah) for _ in range(random.randint(6, 22))])
+            while 'пп' in lol or 'хх' in lol or 'аа' in lol or 'вв' in lol or 'фф' in lol or 'ыы' in lol:
+	            lol = lol.replace('пп', 'п').replace('хх', 'х').replace('аа', 'а').replace('вв', 'в').replace('фф', 'ф').replace('ыы', 'ы')
+            if reply:
+              await reply.reply(lol)
+            else:
+              await app.send_message(message.chat.id, lol)
+        else:
+          return await utils.answer(message, 'Значение должно быть цифрой или значением random')
+    else:
+      return await utils.answer(message, 'Нет аргумента')
