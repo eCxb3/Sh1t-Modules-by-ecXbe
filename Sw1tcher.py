5290090d2e0d13ae0c0fb394f4e4f6cf27527090
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-08 16:14:10 +0300

    Update Sw1tcher.py

--- Sw1tcher.py
+++ Sw1tcher.py
@@ -8,8 +8,8 @@ class SwitcherMod(loader.Module):
   """Меняет раскладку сообщения при случайном написании сообщения на другой раскладке"""
   
   async def switch_cmd(self, app: Client, message: types.Message, args: str):
-    RuKeys = """ёйцукенгшщзхъфывапролджэячсмитьбю.Ё"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"""
-    EnKeys = """`qwertyuiop[]asdfghjkl;'zxcvbnm,./~@#$%^&QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?"""
+    Ru = """ёйцукенгшщзхъфывапролджэячсмитьбю.Ё"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"""
+    En = """`qwertyuiop[]asdfghjkl;'zxcvbnm,./~@#$%^&QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?"""
     
     reply = message.reply_to_message
     if reply:
@@ -24,8 +24,8 @@ class SwitcherMod(loader.Module):
         return await utils.answer(reply, text)
     else:
       if args:
-        change = str.maketrans(RuKeys + EnKeys, EnKeys + RuKeys)
+        change = str.maketrans(Ru + En, En + Ru)
         text = str.translate(args, change)
         return await utils.answer(message, text)
       else:
-        return await utils.answer(message, '''Нет реплая и аргумента,   использование -switch <replay/text>''')
+        return await utils.answer(message, """Нет реплая и аргумента, использование -switch <replay/text>""")
