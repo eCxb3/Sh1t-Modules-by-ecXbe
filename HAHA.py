bf17783fb1b451d997a11ebc26baa17d6664aa98
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-08 16:09:58 +0300

    Update HAHA.py

--- HAHA.py
+++ HAHA.py
@@ -12,7 +12,7 @@ class NotesMod(loader.Module):
     
     """Использовать -haha <кол-во/random>"""
     
-    repl = message.reply_to_message
+    reply = message.reply_to_message_id
     await message.delete()
     hah = ['п', 'х', 'а', 'в', 'ф', 'ы'] # 22
     if args:
@@ -21,22 +21,22 @@ class NotesMod(loader.Module):
           lol = "".join([random.choice(hah) for _ in range(random.randint(6, 22))])
           while 'пп' in lol or 'хх' in lol or 'аа' in lol or 'вв' in lol or 'фф' in lol or 'ыы' in lol:
             lol = lol.replace('пп', 'п').replace('хх', 'х').replace('аа', 'а').replace('вв', 'в').replace('фф', 'ф').replace('ыы', 'ы')
-          if repl:
-            await repl.reply(lol)
+          if reply:
+            await app.send_message(message.chat.id, lol, reply_to_message_id=reply)
           else:
             await app.send_message(message.chat.id, lol)
-          await sleep(random.uniform(0.1, 1.5))
+          await sleep(random.uniform(0.1, 0.2))
       else:
         if args == "random":
           for _ in range(random.randint(2, 10)):
             lol = "".join([random.choice(hah) for _ in range(random.randint(6, 22))])
             while 'пп' in lol or 'хх' in lol or 'аа' in lol or 'вв' in lol or 'фф' in lol or 'ыы' in lol:
               lol = lol.replace('пп', 'п').replace('хх', 'х').replace('аа', 'а').replace('вв', 'в').replace('фф', 'ф').replace('ыы', 'ы')
-            if repl:
-              await repl.reply(lol)
+            if reply:
+              await app.send_message(message.chat.id, lol, reply_to_message_id=reply)
             else:
               await app.send_message(message.chat.id, lol)
-            await sleep(random.uniform(0.1, 1.5))
+            await sleep(random.uniform(0.1, 0.2))
         else:
           return await utils.answer(message, 'Значение должно быть цифрой или значением random')
     else:
