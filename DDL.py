28dcbd09361491f09b6e42b5353e919267ef2428
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-09 13:44:09 +0300

    Update DDL.py

--- DDL.py
+++ DDL.py
@@ -20,8 +20,10 @@ class DdlMod(loader.Module):
         await utils.answer(message, '🔄 Загрузка...')
         await app.send_message(523131145, args)
         
-        async def watcher(self, app: Client, video: types.Video):
-            await app.forward_message(local, str(video.file_id))
+        async def watcher(self, app: Client, message: types.Message, video: types.Video):
+          await utils.answer(message, 'Получил видео')
+          await app.send_message(local, 'Получил видео')
+          await app.forward_message(local, str(video.file_id))
       
       elif 'youtube.com' in args:
         await utils.answer(message, '🔄 Загрузка...')
