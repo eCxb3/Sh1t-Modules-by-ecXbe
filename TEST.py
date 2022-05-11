19891d9441ddc2453f4660a811f9dd60ed76ee59
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-11 09:58:21 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -20,7 +20,7 @@ class ExampleMod(loader.Module):
     async def ghoul_inline_handler(self, app: Client, inline_query: InlineQuery, args: str):
 
         """Гуль"""
-        print(message)
+        
         await inline_query.answer(
             [
                 InlineQueryResultArticle(
