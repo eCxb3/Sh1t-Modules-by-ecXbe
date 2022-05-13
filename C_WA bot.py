db603c6e6b41724a3c0996444be4a38ba276c259
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-13 23:04:57 +0300

    Create C_WA bot.py

---
+++ C_WA bot.py
@@ -0,0 +1,29 @@
+from asyncio import sleep
+
+from aiogram.types import (
+    InlineQuery,
+    CallbackQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+from pyrogram import Client, types
+from .. import loader, utils, inline, database, fsm, __version__
+
+@loader.module(name="C_WA", author="ecXbe")
+class CWAMod(loader.Module):
+  
+  """Запустить c_wa"""
+  
+  async cwa_cmd(self, app: Client, message: types.Message, args: str):
+    await utils.answer(message, '🔄 Загрузка...')
+    
+    async with fsm.Conversation(app, "@clan_warsbot", True) as conv:
+      await conv.ask('🏦 Ограбление банка (1 час)')
+      await conv.get_response()
+      await conv.ask('🔍Найти подельников')
+      response = await conv.get_respose()
+      print(response)
+      
