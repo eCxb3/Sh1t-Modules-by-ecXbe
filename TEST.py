46c7c32e3e4bf51513c275b0ec8cd2cc8b92fb04
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-14 16:41:21 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -35,5 +35,5 @@ from .. import loader, utils, __version__
 class TestMod(loader.Module):
     
   async def test_cmd(self, app: Client, message: types.Message):  
-    history = await app.get_chat_history(chat_id="@clan_warsbot", limit=1)
-    await app.send_message(history.text)
+    history = await app.get_messages(chat_id="@clan_warsbot", limit=1)
+    await app.send_message(history[0].text)
