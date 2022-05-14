d7928c37b7fec10e0c4b6111e0258239983bb630
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-14 22:03:25 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -34,14 +34,14 @@ class GhoulforallMod(loader.Module):
     @loader.on_bot(lambda self, app, call: call.data == "ghoul_button_callback")  # Сработает только если каллбек дата равняется "example_button_callback"
     async def example_callback_handler(self, app: Client, call: CallbackQuery):  # _callback_handler на конце функции чтобы обозначить что это каллбек-хендлер
         """Пример каллбека"""
-        await self.bot.send_message(inline_message_id=call.inline_message_id, text='Я гуль')
+        await self.bot.edit_message_text(inline_message_id=call.inline_message_id, text='Я гуль')
         await sleep(2)
         a = 1000
         while a > 0:
           c = a - 7
-          await self.bot.send_message(inline_message_id=call.inline_message_id, text=str(a) + " - 7 = " + str(c))
+          await self.bot.edit_message_text(inline_message_id=call.inline_message_id, text=str(a) + " - 7 = " + str(c))
           a = c
           await sleep(0.1)
-        await self.bot.send_message(inline_message_id=call.inline_message_id, text='l l let me die')
+        await self.bot.edit_message_text(inline_message_id=call.inline_message_id, text='l l let me die')
     
 
