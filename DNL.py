c96972e66708f8a2a00010a49334030680a4f52d
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-15 20:36:11 +0300

    Update DNL.py

--- DNL.py
+++ DNL.py
@@ -61,3 +61,57 @@ class DnlMod(loader.Module):
           await app.send_video(local, str(response.video.file_id))
     else:
       return await utils.answer(message, '❌ Ссылка не найдена')
+
+
+  async def dlv_message_handler(self, app: Client, message: Message, args: str):
+    
+    reply = message.reply_to_message
+    local = message.chat.id
+    if args:
+      link = args
+    elif reply:
+      if not reply.text:
+        return await utils.answer(message, '❌ В реплае нет текста')
+      else:
+        link = reply.text
+    else:
+      return await utils.answer(message, '❌ Нет аргумента и реплая')
+    
+    if 'tiktok.com' in link:
+      await utils.answer(message, '🔄 Загрузка...')
+        
+      async with fsm.Conversation(app, "@downloader_tiktok_bot", True) as conv:
+        try:
+          await conv.ask(link)
+        except:
+          await app.unblock_user("@downloader_tiktok_bot")
+          await conv.ask(link)
+        try:
+          response = await conv.get_response(60)
+        except:
+          return await utils.answer(message, '❌ Превышено время ожидания')
+        await message.delete()
+        if reply:
+          await app.send_video(local, str(response.video.file_id), reply_to_message_id=message.reply_to_message_id)
+        else:
+          await app.send_video(local, str(response.video.file_id))
+    elif 'youtube.com' in link or 'youtu.be' in link:
+      await utils.answer(message, '🔄 Загрузка...')
+        
+      async with fsm.Conversation(app, "@youtubednbot", True) as conv:
+        try:
+          await conv.ask(link)
+        except:
+          await app.unblock_user("@youtubednbot")
+          await conv.ask(link)
+        try:
+          response = await conv.get_response(60)
+        except:
+          return await utils.answer(message, '❌ Превышено время ожидания')
+        await message.delete()
+        if reply:
+          await app.send_video(local, str(response.video.file_id), reply_to_message_id=message.reply_to_message_id)
+        else:
+          await app.send_video(local, str(response.video.file_id))
+    else:
+      return await utils.answer(message, '❌ Ссылка не найдена')
