27df538b0d304ee13e61de135a1fa94541f3a340
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-16 17:58:07 +0300

    Update DNL.py

--- DNL.py
+++ DNL.py
@@ -115,6 +115,11 @@ class DnlMod(loader.Module):
         except:
           return await utils.answer(loading, '❌ Превышено время ожидания')
         await loading.delete()
-        await self.bot.send_video(chat_id=message.chat.id, video=str(response.reply_to_message.text))
+        
+        bt = await app.send_video('@sh1tub_8VVd1k_bot', video=str(response.video.file_id))
+        
+        await self.bot.copy_message(chat_id=2005298859, from_chat_id=bt.chat.id, message_id=bt.message_id)
+        #await self.bot.send_video(chat_id=message.chat.id, video=str(response.reply_to_message.text))
+        await bt.delete()
     else:
       return await message.reply('❌ Ссылка не найдена')
