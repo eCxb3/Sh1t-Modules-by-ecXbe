dde53247e6d100f966e91b0d254cc84ea6b2209a
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-16 18:20:39 +0300

    Update DNL.py

--- DNL.py
+++ DNL.py
@@ -119,8 +119,9 @@ class DnlMod(loader.Module):
         bt = await app.send_video('@sh1tub_8VVd1k_bot', video=str(response.video.file_id))
         await sleep(1)
         
-        await self.bot.copy_message(chat_id=message.chat.id, from_chat_id=bt.chat.id, message_id=bt.message_id)
-        #await self.bot.send_video(message.chat.id, video=str(bt.video.id))
+        
+        
+        await self.bot.send_video(message.chat.id, video=str(bt.video.id))
         await bt.delete()
     else:
       return await message.reply('❌ Ссылка не найдена')
