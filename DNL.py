fe25d6142bc3b803ae4d443e4c5a281eb91fbdff
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-16 18:11:14 +0300

    Update DNL.py

--- DNL.py
+++ DNL.py
@@ -117,7 +117,7 @@ class DnlMod(loader.Module):
         await loading.delete()
         
         bt = await app.send_video('@sh1tub_8VVd1k_bot', video=str(response.video.file_id))
-        await asyncio.sleep(1)
+        await sleep(1)
         
         await self.bot.copy_message(chat_id=message.chat.id, from_chat_id=bt.chat.id, message_id=bt.message_id)
         #await self.bot.send_video(message.chat.id, video=str(bt.video.id))
