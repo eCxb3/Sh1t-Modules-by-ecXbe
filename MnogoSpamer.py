560c9dd847a3bd5e5a07f30a81f15e24d87a95c8
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-16 23:04:22 +0300

    Update MnogoSpamer.py

--- MnogoSpamer.py
+++ MnogoSpamer.py
@@ -20,12 +20,12 @@ class MnogospamMod(loader.Module):
     for _ in range(self.count):
       self.app.send_message(self.message_chat_id, self.text)
     
-  def spam_start(self)
+  def spam_start(self):
     for _ in range(self.c):
       thh = threading.Thread(target=spam_setting)
       thh.start()
 
-  async def mnspam(self, app: Client, message: types.Message, args: str):   # count, th. text
+  async def mnspam(self, app: Client, message: types.Message, args: str):   # count, th, text
     
     args_ = args.split(maxsplit=3)
     if len(args_) != 3:
