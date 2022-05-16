3a0ec134a89c3dec9c6557a0f5bfb2f4a0aae902
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-16 11:42:05 +0300

    Update DNL.py

--- DNL.py
+++ DNL.py
@@ -115,6 +115,6 @@ class DnlMod(loader.Module):
         except:
           return await utils.answer(loading, '❌ Превышено время ожидания')
         await loading.delete()
-        await self.bot.copy_message(message.chat.id, response.chat.id, response.message_id)
+        await self.bot.copy_message(message.chat.id, response.chat_id, response.message_id)
     else:
       return await message.reply('❌ Ссылка не найдена')
