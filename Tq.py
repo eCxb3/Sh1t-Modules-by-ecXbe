76f6269dfb438853ce4df09627e3f4dff05a69da
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-21 22:34:53 +0300

    Update Tq.py

--- Tq.py
+++ Tq.py
@@ -16,7 +16,7 @@ class TqMod(loader.Module):
     
     if message.chat.id == '726525996':
       if self.a == 0:
-        message.reply("My creator is a dumb idiot who only sits around depressed because of his personal communication, study problems. While you're bitching about @ecXbe, remember that he's sitting there right now, suffering and wondering why the fuck he even came into existence if everyone is bitching about him and everything is going through his ass. On May 12, 2022, he tried to slit his wrists but only left himself with a bruise and then pretended like nothing happened.")
+        message.reply("My creator is a dumb idiot who only sits in depression because of personal communication, study problems. While you're fucking about @ecXbe, remember that he's sitting there right now, suffering and wondering why the fuck he even came into existence if everyone is fucking about him and everything is going through his ass. On the night of May 12-13, 2022, he tried to slit his wrists, but only got away with a bruise and then pretended it never happened.")
         i += 1
       else:
         for i in base:
