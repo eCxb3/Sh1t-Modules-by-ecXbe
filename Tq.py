d1bf5e5cb41e87b90be85df829ed5672990f4532
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-21 22:19:39 +0300

    Update Tq.py

--- Tq.py
+++ Tq.py
@@ -14,6 +14,7 @@ class TqMod(loader.Module):
      # self.app.send_message(message.chat.id, "Ooh ooh my creator, don't overdo it.")
     
     if message.chat.id == '726525996':
+      app.send_message(message.chat.id, "Let's give it a try.")
       if a == 0:
         message.reply("My creator is a dumb idiot who only sits around depressed because of his personal communication, study problems. While you're bitching about @ecXbe, remember that he's sitting there right now, suffering and wondering why the fuck he even came into existence if everyone is bitching about him and everything is going through his ass. On May 12, 2022, he tried to slit his wrists but only left himself with a bruise and then pretended like nothing happened. Now is not a good time for him to talk.")
         i += 1
