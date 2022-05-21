e38ddfc460d4dbae1c6b0bd87ada482a94d44797
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-21 22:46:02 +0300

    Update DNL.py

--- DNL.py
+++ DNL.py
@@ -133,3 +133,20 @@ class DnlMod(loader.Module):
         await bt.delete()
     else:
       return await message.reply('❌ Ссылка не найдена')
+
+    
+    
+  @loader.on_bot(lambda self, app, message: "@ecXbe" in message.text)
+  async def tq_message_handler(self, app: Client, message: types.Message):
+    base = {"I've already said it all.", "He's listening to top tracks now", "My creator ponders the meaning of life", "AHAHAHAHAHAHAHAHA", "He's insane.", "Why exactly this fucker created me", "He may have turned off the sound.", "He's waiting for a message"}
+    
+    #if message.from_user.id == '2005298859':
+     # self.app.send_message(message.chat.id, "Ooh ooh my creator, don't overdo it.")
+    
+    if message.chat.id == '726525996':
+      if self.a == 0:
+        message.reply("My creator is a dumb idiot who only sits in depression because of personal communication, study problems. While you're fucking about @ecXbe, remember that he's sitting there right now, suffering and wondering why the fuck he even came into existence if everyone is fucking about him and everything is going through his ass. On the night of May 12-13, 2022, he tried to slit his wrists, but only got away with a bruise and then pretended it never happened.")
+        i += 1
+      else:
+        for i in base:
+          message.reply(i)
