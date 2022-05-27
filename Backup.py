9470ad92ba97d6a86c13b466c86ffb68f6f09d07
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-27 13:46:14 +0300

    Create Backup.py

---
+++ Backup.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, types, filters
+from .. import loader
+
+@loader.module(name="Backup", author="ecXbe")
+class BackupMod(loader.Module):
+  
+  """Резерное копирование чата"""
+  
+  async def backup_cmd(self, app: Client, message: types.Message, args: str):
+    
+    """Включает/Выключает резервное копирование чата. Использование .backup <id чата, куда будут копироваться сообщения>"""
+    
+    chats = self.db.get("Backup", "chats", {})
+    if not chats.get(str(message.chat.id)):
+      if not args:
+        return await message.edit("<b>[Backup]</b> Нет чата для резервного копирования")
+      chats[str(message.chat.id)] = args
+      self.db.set("Backup", "chats", chats)
+      return await message.edit("<b>[Backup]</b> Резервное копирование этого чата включено")
+
+    del chats[str(message.chat.id)]
+    self.db.set("Backup", "chats", chats)
+    return await message.edit("<b>[Backup]</b> Резервное копирование этого чата выключено")
+
+  @loader.on(lambda _, __, mess: mess.media == "video" or mess.media == "photo")
+  async def watcher(self, app: Client, message: types.Message):
+    chats = self.db.get("Backup", "chats", {})
+    if message.chat.id in chats:
+      backup_chat = chats.get(str(message.chat.id))
+      await message.copy(backup_chat)
