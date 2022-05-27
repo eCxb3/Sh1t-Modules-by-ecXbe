f92d83dd37595b4b2369c8abe628c5b941729f50
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-05-27 10:40:07 +0300

    Update Tq.py

--- Tq.py
+++ Tq.py
@@ -17,35 +17,4 @@ class MutebMod(loader.Module):
     async def watcher(self, app: Client, message: types.Message):
         """Принт"""
         if message.chat.id in self.db.get("MuteB", "chats", []):
-            return await utils.answer(message, "Пользователь добавил вас в чёрный список. Походу вы ему чем-то насолили")
-    
-    async def mutebc_cmd(self, app: Client, message: types.Message, args: str):
-        
-        chat = message.chat
-        
-        chats = self.db.get("MuteBot", "botc", {})
-        if not chats.get(str(chat.id)):
-            chats[str(chat.id)] = 0
-            self.db.set("MuteBot", "botc", chats)
-            return await utils.answer(
-                message, "Bot started, creator sad inside")
-
-        del chats[str(chat.id)]
-        self.db.set("MuteBot", "botc", chats)
-        return await utils.answer(
-                message, "Bot finished, creator sad inside")
-   
-    @loader.on(~filters.me)
-    async def muteb_message_handler(self, app: Client, message: types.Message):
-        base = {"I've already said it all.", "He's listening to top tracks now", "My creator ponders the meaning of life", "AHAHAHAHAHAHAHAHA", "He's insane.", "Why exactly this fucker created me", "He may have turned off the sound.", "He's waiting for a message"}
-        if message.chat.id in self.db.get("MuteBot", []):
-            if message.from_user.id == 2005298859:
-                return await message.reply("Ooh ooh my creator, don't overdo it.")
-            else:
-                if self.db.get("MuteB", "botc", message.chat.id) == 0:
-                    return await message.reply("My creator is a dumb idiot who only sits in depression because of personal communication, study problems. While you're fucking about @ecXbe, remember that he's sitting there right now, suffering and wondering why the fuck he even came into existence if everyone is fucking about him and everything is going through his ass. On the night of May 12-13, 2022, he tried to slit his wrists, but only got away with a bruise and then pretended it never happened.")
-                    botc = self.db.get("MuteBot", "botc")
-                    self.db.set("MuteBot", "botc", list({*botc} ^ {message.chat.id: 1}))
-                else:
-                    for i in base:
-                        return await message.reply(i)
+            return await app.send_video(message.chat.id, "BAACAgIAAx0EYY6MsAACFj9ikH-2u901yj1K4sLyGkYo4QlLdAACchYAApzWiEgh-qigp-UKnR4E")
