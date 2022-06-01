afa64cddb3d0a23e26a59002f180bb91ac0167d5
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-06-01 22:57:25 +0300

    Update Bot ecXbe.py

--- Bot ecXbe.py
+++ Bot ecXbe.py
@@ -138,6 +138,8 @@ class ExbotMod(loader.Module):
         index += 1
       if range[0].isdigit() == False or range[1].isdigit() == False:
         return await message.reply("❌ Диапазон должен быть целыми числами")
+      if int(range[1]) > int(range[0]):
+        return await message.reply("❌ Первое число должно быть меньше второго")
       return await message.reply(randint(int(range[0]), int(range[1])))  
     else:
       return await message.reply("❌ Отсутствует диапазон")
